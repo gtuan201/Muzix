4723623bbdea65a4b47e8a19746b3209d8fc88a4
gtuan201 <106383317+gtuan201@users.noreply.github.com>
2023-05-01 09:59:45 +0700

    Muzix

--- app/src/main/java/com/example/muzix/service/PlayMusicService.kt
+++ app/src/main/java/com/example/muzix/service/PlayMusicService.kt
@@ -8,44 +8,44 @@ import android.graphics.BitmapFactory
 import android.media.MediaPlayer
 import android.net.Uri
 import android.os.Bundle
-import android.os.Handler
 import android.os.IBinder
-import android.support.v4.media.session.MediaSessionCompat
 import android.util.Log
 import android.widget.RemoteViews
 import androidx.core.app.NotificationCompat
-import androidx.core.app.NotificationManagerCompat
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.example.muzix.R
 import com.example.muzix.model.Song
 import com.example.muzix.ultis.ControllerMusicReceiver
 import com.example.muzix.ultis.NotificationApplication.Companion.NOTIFICATION_CHANNEL
 
-class PlayMusicService: Service() {
-    private var mediaPlayer : MediaPlayer? = null
-    private var isPlaying : Boolean = false
-    private var song : Song? = null
+
+class PlayMusicService : Service() {
+    private var mediaPlayer: MediaPlayer? = null
+    private var isPlaying: Boolean = false
+    private var song: Song? = null
+
     companion object {
         const val ACTION_START = 1
         const val ACTION_RESUME = 2
         const val ACTION_PAUSE = 3
         const val ACTION_CLEAR = 4
     }
+
     override fun onBind(intent: Intent?): IBinder? {
         return null
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        if (intent?.action == "play"){
+        if (intent?.action == "play") {
             song = intent.getParcelableExtra("song")
             if (song != null) {
                 startMusic(song)
                 sendNotification(song)
             }
         }
-        val actionMusic = intent?.getIntExtra("action_service",0)
+        val actionMusic = intent?.getIntExtra("action_service", 0)
         if (actionMusic != null) handleActionMusic(actionMusic)
-        if (mediaPlayer != null){
+        if (mediaPlayer != null) {
             mediaPlayer?.setOnCompletionListener {
                 stopSelf()
             }
@@ -55,14 +55,15 @@ class PlayMusicService: Service() {
     }
 
     private fun startMusic(song: Song?) {
-        if (mediaPlayer == null){
+        if (mediaPlayer == null) {
             mediaPlayer = MediaPlayer.create(this, Uri.parse(song?.mp3))
         }
         mediaPlayer?.start()
         isPlaying = true
         sendActionToActivity(ACTION_START)
     }
-    private fun resumeMusic(){
+
+    private fun resumeMusic() {
         if (!isPlaying && mediaPlayer != null) {
             mediaPlayer?.start()
             isPlaying = true
@@ -70,7 +71,8 @@ class PlayMusicService: Service() {
             sendActionToActivity(ACTION_RESUME)
         }
     }
-    private fun pauseMusic(){
+
+    private fun pauseMusic() {
         if (mediaPlayer != null && isPlaying) {
             mediaPlayer?.pause()
             isPlaying = false
@@ -81,47 +83,80 @@ class PlayMusicService: Service() {
 
     private fun sendNotification(song: Song?) {
         val remoteViews = RemoteViews(packageName, R.layout.notification)
-        remoteViews.setTextViewText(R.id.notification_title,song?.name)
-        remoteViews.setTextViewText(R.id.notification_artist,song?.artist)
-        if (isPlaying){
-            remoteViews.setOnClickPendingIntent(R.id.notification_play_pause,getPendingIntent(this, ACTION_PAUSE))
-            remoteViews.setImageViewResource(R.id.notification_play_pause,R.drawable.ic_pause)
+        remoteViews.setTextViewText(R.id.notification_title, song?.name)
+        remoteViews.setTextViewText(R.id.notification_artist, song?.artist)
+        if (isPlaying) {
+            remoteViews.setOnClickPendingIntent(
+                R.id.notification_play_pause,
+                getPendingIntent(this, ACTION_PAUSE)
+            )
+            remoteViews.setImageViewResource(R.id.notification_play_pause, R.drawable.ic_pause)
+        } else {
+            remoteViews.setOnClickPendingIntent(
+                R.id.notification_play_pause,
+                getPendingIntent(this, ACTION_RESUME)
+            )
+            remoteViews.setImageViewResource(R.id.notification_play_pause, R.drawable.ic_play)
         }
-        else{
-            remoteViews.setOnClickPendingIntent(R.id.notification_play_pause,getPendingIntent(this, ACTION_RESUME))
-            remoteViews.setImageViewResource(R.id.notification_play_pause,R.drawable.ic_play)
-        }
-        remoteViews.setOnClickPendingIntent(R.id.notification_cancel,getPendingIntent(this, ACTION_CLEAR))
-        val bitmap = BitmapFactory.decodeResource(resources,R.drawable.muzix_logo)
-        val notification = NotificationCompat.Builder(this, NOTIFICATION_CHANNEL)
-            .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
+        remoteViews.setOnClickPendingIntent(
+            R.id.notification_cancel,
+            getPendingIntent(this, ACTION_CLEAR)
+        )
+//        val notification = NotificationCompat.Builder(this, NOTIFICATION_CHANNEL)
+//            .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
+//            .setSmallIcon(R.drawable.baseline_music_note_24)
+//            .setLargeIcon(bitmap)
+//            .setCustomContentView(remoteViews)
+//            .build()
+
+        val notificationBuilder = NotificationCompat.Builder(this, NOTIFICATION_CHANNEL)
             .setSmallIcon(R.drawable.baseline_music_note_24)
-            .setLargeIcon(bitmap)
-            .setCustomContentView(remoteViews)
-            .build()
-        startForeground(1,notification)
+            .setContentTitle(song?.name)
+            .setContentText(song?.artist)
+            .setLargeIcon(BitmapFactory.decodeResource(resources,R.drawable.logo))
+            .setStyle(
+                androidx.media.app.NotificationCompat.MediaStyle()
+                    .setShowActionsInCompactView(0, 1, 2)
+                    .setShowCancelButton(true)
+            )
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+        if (isPlaying) {
+            notificationBuilder.addAction(R.drawable.ic_previous, "Previous", null)
+                .addAction(R.drawable.ic_pause, "Pause", getPendingIntent(this, ACTION_PAUSE))
+                .addAction(R.drawable.ic_next, "Next", null)
+                .addAction(R.drawable.ic_clear,"Clear",getPendingIntent(this, ACTION_CLEAR))
+        }
+        else {
+            notificationBuilder.addAction(R.drawable.ic_previous, "Previous", null)
+                .addAction(R.drawable.ic_play, "Play", getPendingIntent(this, ACTION_RESUME))
+                .addAction(R.drawable.ic_next, "Next", null)
+                .addAction(R.drawable.ic_clear,"Clear",getPendingIntent(this, ACTION_CLEAR))
+        }
+        startForeground(1, notificationBuilder.build())
     }
 
     @SuppressLint("UnspecifiedImmutableFlag")
     private fun getPendingIntent(playMusicService: PlayMusicService, action: Int): PendingIntent? {
-        val intent = Intent(this,ControllerMusicReceiver::class.java)
-        intent.putExtra("action",action)
-        return PendingIntent.getBroadcast(this,action,intent,PendingIntent.FLAG_UPDATE_CURRENT)
+        val intent = Intent(this, ControllerMusicReceiver::class.java)
+        intent.putExtra("action", action)
+        return PendingIntent.getBroadcast(this, action, intent, PendingIntent.FLAG_UPDATE_CURRENT)
     }
-    private fun nextSongRecommend(){
-        if (isSongAlmostFinished()){
-            Log.e("isSongAlmostFinished","true")
+
+    private fun nextSongRecommend() {
+        if (isSongAlmostFinished()) {
+            Log.e("isSongAlmostFinished", "true")
         }
     }
 
-    private fun isSongAlmostFinished() : Boolean{
+    private fun isSongAlmostFinished(): Boolean {
         val currentPosition = mediaPlayer?.currentPosition ?: 0
         val totalDuration = mediaPlayer?.duration ?: 0
         val percentage = (currentPosition.toFloat() / totalDuration.toFloat()) * 100
         return percentage >= 10
     }
-    private fun handleActionMusic(action : Int){
-        when(action){
+
+    private fun handleActionMusic(action: Int) {
+        when (action) {
             ACTION_PAUSE -> pauseMusic()
             ACTION_RESUME -> resumeMusic()
             ACTION_CLEAR -> {
@@ -130,19 +165,19 @@ class PlayMusicService: Service() {
             }
         }
     }
-    private fun sendActionToActivity(action: Int){
+
+    private fun sendActionToActivity(action: Int) {
         val intent = Intent("send_action")
         val bundle = Bundle()
-        bundle.putParcelable("song",song)
-        bundle.putBoolean("isPlaying",isPlaying)
-        bundle.putInt("action",action)
+        bundle.putParcelable("song", song)
+        bundle.putBoolean("isPlaying", isPlaying)
+        bundle.putInt("action", action)
         intent.putExtras(bundle)
         LocalBroadcastManager.getInstance(this).sendBroadcast(intent)
     }
-
     override fun onDestroy() {
         super.onDestroy()
-        if (mediaPlayer != null){
+        if (mediaPlayer != null) {
             mediaPlayer?.stop()
             mediaPlayer?.release()
             mediaPlayer = null
