3413f0e67d13530001b8582ab131e8c8129e8dd1
gtuan201 <106383317+gtuan201@users.noreply.github.com>
2023-06-08 17:05:05 +0700

    Muzix

--- app/src/main/java/com/example/muzix/view/premium/PremiumFragment.kt
+++ app/src/main/java/com/example/muzix/view/premium/PremiumFragment.kt
@@ -9,10 +9,6 @@ import com.example.muzix.R
 
 class PremiumFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
